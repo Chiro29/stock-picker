c47bf6262143f4e69ff91eb6dd354393c5db5d58
chiro <80062293+Chiro29@users.noreply.github.com>
2024-11-21 15:21:51 +0100

    feat(project): added a loop so that the user can decide when to stop the programme and the input to generate a number of days

--- stone_picker.rb
+++ stone_picker.rb
@@ -32,4 +32,21 @@ def stock_picker(array)
   puts "Best day to buy is day #{best_day_buy + 1} with the value of #{best_buy}€\nBest day to sell is day #{best_day_sell + 1} with the value of #{best_sell}€\nThe profit is #{max_profit}€"
 end
 
-stock_picker(stock)
+loop do
+  puts "Choose how many days you want to have for the stock picking that will be generated\nIt will show you the best day to buy, to sell and the profit"
+  days = gets.chomp
+
+  until days.match?(/\A\d+\z/) do
+    puts "To choose a number of days, insert numbers only"
+    days = gets.chomp
+  end
+
+  stock_picker(stock)
+
+  puts "Do you want to continue using the programme?\nPress any key other than 'n' or enter to continue\n'n' to close"
+  again = gets.chomp.downcase
+
+  if again === "n"
+    break
+  end  
+end
